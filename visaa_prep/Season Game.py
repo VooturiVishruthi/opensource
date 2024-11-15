c868f966cb7e72b4170b1f31bfdd0cb5a05db498
VooturiVishruthi <151535326+VooturiVishruthi@users.noreply.github.com>
2024-11-15 12:06:08 +0530

    Create Season Game.py

---
+++ visaa_prep/Season Game.py
@@ -0,0 +1,11 @@
+a=int(input())
+if(a>=3 and a<=5):
+   print("Spring")
+if(a>=6 and a<=8):
+   print("Summer")
+if(a>=9 and a<=11):
+   print("Autumn")
+if(a==12 or (a>=1 and a<=2)):
+   print("Winter")
+if(a>12):
+   print("Invalid")
